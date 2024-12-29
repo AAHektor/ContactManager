1edf618378eed34f7966ba6abf21b24aa57f8afd
Aron Hektor <aronhektor@hotmail.com>
2024-12-29 17:58:14 +0100

    Add project files.

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace ContactManager.Models;
+
+public class Contact
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public int Number { get; set; }
+    public string Address { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+
+    public Guid Id { get; set; }
+
+    public Contact()
+    {
+        Id = Guid.NewGuid();
+    }
+
+}
---
+++ ContactManager/Services/MenuDialogs.cs
@@ -0,0 +1,41 @@
+﻿namespace ContactManager.Services;
+
+public class MenuDialogs
+{
+    public void CreateUserDialog()
+    {
+        bool running = true;
+
+        while (running)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to ContactManager\n");
+            Console.WriteLine("1. Contacts");
+            Console.WriteLine("2. Create Contact");
+            Console.WriteLine("3. Exit Program");
+            Console.WriteLine();
+
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    ContactList();
+                    break;
+
+                case "2":
+                    ContactCreate();
+                    break;
+
+                case "3":
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("invalid option.");
+                    break;
+            }
+        }
+    }
+
+}
